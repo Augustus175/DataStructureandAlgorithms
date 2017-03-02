a0d18ae55db047b2f94d8292bcc1df9096890166
zhangzhibo <columbia@163.com>
2017-03-02 09:25:46 +0800

    2017-013-02 09:25

---
+++ src/chap03/BubbleSortApp.java
@@ -0,0 +1,22 @@
+package chap03;
+
+/**
+ * Created by zhangzhibo on 17-3-2.
+ */
+public class BubbleSortApp {
+    public static void main(String[] args) {
+        ArrayBub arr = new ArrayBub(10);
+        arr.insert(12);
+        arr.insert(10);
+        arr.insert(1);
+        arr.insert(2);
+        arr.insert(9);
+        arr.insert(8);
+        arr.insert(15);
+        arr.insert(0);
+        arr.insert(3);
+        arr.display();
+        arr.bubbleSort();
+        arr.display();
+    }
+}
