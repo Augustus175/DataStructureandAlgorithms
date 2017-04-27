6fbbcffad89433594000be6d39581246ee293539
zhangzhibo <columbia@163.com>
2017-04-27 16:07:58 +0800

    2017.04.27 16:07

--- src/OctoberAlgorithms/chap01a/ResersePolish.java
+++ src/OctoberAlgorithms/chap01a/ResersePolish.java
@@ -1,118 +1,21 @@
 package OctoberAlgorithms.chap01a;
 
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Created by zhangzhibo on 2017/4/27.
  */
-class Node {
-    private String iData;
-    private Node leftChild = null;
-    private Node rightChild = null;
 
-    public Node(int String) {
-        this.iData = iData;
-    }
 
-    public void setLeftChild(Node leftChild) {
-        this.leftChild = leftChild;
-    }
-
-    public void setRightChild(Node rightChild) {
-        this.rightChild = rightChild;
-    }
-
-    public Node getLeftChild() {
-        return leftChild;
-    }
-
-    public Node getRightChild() {
-        return rightChild;
-    }
-
-    public String getiData() {
-        return this.iData;
-    }
-    public void displayNode()
-    {
-        System.out.print(iData+" ");
-    }
-}
-
-class Tree {
-    Node root;
-
-    public void insert(int iData) {
-        Node node = new Node(iData);
-        if (root == null) {
-            root = node;
-        } else {
-            Node current = root;
-            Node paraent = root;
-            while (true) {
-                paraent = current;
-                if (iData < current.getiData()) {
-                    current = current.getLeftChild();
-                    if (current == null) {
-                        paraent.setLeftChild(node);
-                        return;
-                    }
-                } else {
-                    current = current.getRightChild();
-                    if (current == null) {
-                        paraent.setRightChild(node);
-                        return;
-                    }
-                }
-            }
-        }
-
-    }
-
-    public void preOrder(Node node) {
-        if (node!=null)
-        {
-            node.displayNode();
-            preOrder(node.getLeftChild());
-            preOrder(node.getRightChild());
-        }
-    }
-    public void midOrder(Node node)
-    {
-        if (node!=null)
-        {
-            midOrder(node.getLeftChild());
-            node.displayNode();
-            midOrder(node.getRightChild());
-        }
-    }
-    public void posOrder(Node node) {
-        if (node!=null)
-        {
-            posOrder(node.getLeftChild());
-            posOrder(node.getRightChild());
-            node.displayNode();
-
-        }
-    }
-    public void preOrder()
-    {
-        this.preOrder(this.root);
-    }
-    public void midOrder()
-    {
-        this.midOrder(this.root);
-    }
-    public void posOrder()
-    {
-        this.posOrder(this.root);
-    }
-}
 
 public class ResersePolish {
     public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push("a");
+
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         String tmpstr = new String(str);
---
+++ src/OctoberAlgorithms/chap01a/test.java
@@ -0,0 +1,47 @@
+package OctoberAlgorithms.chap01a;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by zhangzhibo on 17-4-27.
+ */
+public class test {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        String str = "1+2+5-3+6";
+        StringBuilder sb = null;
+        char[] strchr = str.toCharArray();
+        for (int i = 0; i < strchr.length; i++) {
+            char a = strchr[i];
+            if (a >= 48 || a <= 57) {
+                sb.append(a);
+            }else{
+                if (stack.isEmpty()) {
+                    stack.push(a);
+                }else{
+                    char tmp = (char)stack.peek();
+                    if ()
+
+                }
+            }
+        }
+
+    }
+
+    public static boolean compearOpti(char a, char b) {
+        int i = -1;
+        int j = -1;
+        if ((a == '+' || a == '-')) {
+            i = 1;
+        } else {
+            i = 2;
+        }
+        if ((b == '+' || b == '-')) {
+            j = 1;
+        } else {
+            j = 2;
+        }
+        return i > j;
+    }
+}
