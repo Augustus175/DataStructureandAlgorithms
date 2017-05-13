0fe38ba630994f2936767ed757057e4228139c0a
zhangzhibo <columbia@163.com>
2017-05-13 14:02:59 +0800

    2017.05.04 21:37

--- src/OctoberAlgorithms/chap01b/CalcCountApp.java
+++ src/OctoberAlgorithms/chap01b/CalcCountApp.java
@@ -11,7 +11,7 @@ public class CalcCountApp {
     public static int CalcCount(int n) {
         int nNonRepeat = 3;
         int nRepeat = 0;
-        for (int i = 2; i <n ; i++) {
+        for (int i = 2; i <=n ; i++) {
             int t = nNonRepeat;
             nNonRepeat = 2*nNonRepeat+2*nRepeat;
             nRepeat = t;
--- src/OctoberAlgorithms/chap01b/CalcCountApp02.java
+++ src/OctoberAlgorithms/chap01b/CalcCountApp02.java
@@ -5,16 +5,17 @@ package OctoberAlgorithms.chap01b;
  */
 public class CalcCountApp02 {
     public static void main(String[] args) {
-        int[] a = {2, 1, 2, 0};
-        int[] result = MatrixN(a, 4);
-        System.out.println(3 * result[0]);
-        System.out.println(3 * result[1]);
-        for (int i : result) {
-            System.out.print(i + " ");
-        }
-    }
+        int[] a = {2, 2, 1, 0};
+        theMatrixN(a,4);
+        System.out.println(3*a[0]+3*a[2]);
+//        System.out.println(3 * a[0]);
+//        System.out.println(3 * a[2]);
+//        for (int i : a) {
+//            System.out.print(i + " ");
+//        }
 
-    public static int[] MatrixMulti(int[] x, int[] y) {
+    }
+    public static void MatrixMulti(int[] x, int[] y) {
         int x11 = x[0];
         int x12 = x[1];
         int x21 = x[2];
@@ -23,33 +24,34 @@ public class CalcCountApp02 {
         int y12 = y[1];
         int y21 = y[2];
         int y22 = y[3];
-        int[] result = new int[4];
-        result[0] = x11 * y11 + x12 * y21;
-        result[1] = x11 * y12 + x12 * y22;
-        result[2] = x21 * y11 + x22 * y21;
-        result[3] = x21 * y12 + x22 * y22;
-        return result;
-    }
 
-    public static int[] MatrixN(int[] x, int n) {
-//        int[] result = {1,0,0,1};
-        int[] result = new int[4];
+        x[0] = x11 * y11 + x12 * y21;
+        x[1] = x11 * y12 + x12 * y22;
+        x[2] = x21 * y11 + x22 * y21;
+        x[3] = x21 * y12 + x22 * y22;
+    }
+    public static void MatrixN(int[] x, int n, int[] tmp) {
         if (n == 0) {
-            result[0] = 1;
-            result[1] = 0;
-            result[2] = 0;
-            result[3] = 1;
+            x[0]=1;
+            x[1]=0;
+            x[2]=0;
+            x[3]=1;
         } else if (n == 1) {
-            result = x;
+            x[0]=tmp[0];
+            x[1]=tmp[1];
+            x[2]=tmp[2];
+            x[3]=tmp[3];
         } else if (n % 2 == 0) {
-            MatrixN(x, n / 2);
-            result = MatrixMulti(x, x);
+            MatrixN(x, n / 2, tmp);
+            MatrixMulti(x, x);
         } else {
-            int[] t = {x[0], x[1], x[2], x[3]};
-            int[] tmp1 = MatrixN(x, n / 2);
-            int[] tmp2 = MatrixMulti(tmp1, tmp1);
-            result = MatrixMulti(tmp2, t);
+            MatrixN(x, n / 2, tmp);
+            MatrixMulti(x, x);
+            MatrixMulti(x, tmp);
         }
-        return result;
+    }
+    public static void theMatrixN(int[] x, int n) {
+        int[] tmp = {x[0],x[1],x[2],x[3]};
+        MatrixN(x,n,tmp);
     }
 }
