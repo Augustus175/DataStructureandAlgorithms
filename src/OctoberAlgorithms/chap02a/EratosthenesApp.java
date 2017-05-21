37554ea4a3a961f400cb25a8b4bf38a7d4462cf0
zhangzhibo <columbia@163.com>
2017-05-21 22:21:22 +0800

    2017.05.21 22:37

---
+++ src/OctoberAlgorithms/chap02a/EratosthenesApp.java
@@ -0,0 +1,36 @@
+package OctoberAlgorithms.chap02a;
+
+/**
+ * Created by zhangzhibo on 17-5-21.
+ */
+public class EratosthenesApp {
+    public static void main(String[] args) {
+        boolean[] num = new boolean[100];
+        Eratosthene(num);
+        for (int i = 0; i < num.length; i++) {
+            if (!num[i]) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static void Eratosthene(boolean[] a) {
+        int p= 2;
+        int j = p*p;
+        while(j<a.length)
+        {
+            while (j<a.length)
+            {
+                a[j] =true;
+                j+=p;
+            }
+            p++;
+            while (a[p])
+            {
+                p++;
+            }
+            j = p*p;
+        }
+    }
+}
