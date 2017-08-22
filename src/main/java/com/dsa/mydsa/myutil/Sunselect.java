95c250b127a35e0b996ddffb7f745de4d08bbe55
Augustus175 <columbia7311@163.com>
2017-08-22 22:36:52 +0800

    2017.08.22 22:20

---
+++ src/main/java/com/dsa/mydsa/myutil/Sunselect.java
@@ -0,0 +1,50 @@
+package com.dsa.mydsa.myutil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by zhangzhibo-dell on 17-8-22.
+ */
+public class Sunselect {
+    public static void main(String[] args) {
+        Random ran = new Random(10);
+        int[] a = new int[6];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = ran.nextInt(10);
+        }
+        ArrayList<String> result = new ArrayList<>();
+        result = select(a, 3,result);
+        for (String s :
+                result) {
+            System.out.println(s);
+        }
+    }
+
+    private static ArrayList<String> select(int[] a, int k,ArrayList<String> result) {
+        int[] tmp = new int[k];
+        return subselect(a, 0, 1, tmp, k,result);
+    }
+
+    private static ArrayList<String> subselect(int[] a, int head, int index, int[] r, int k, ArrayList<String> result) {
+        for (int i = head; i < a.length + index - k; i++) {
+            if (index < k) {
+                r[index - 1] = a[i];
+                subselect(a, i + 1, index + 1, r, k, result);
+            } else if (index == k) {
+                r[index - 1] = a[i];
+                StringBuffer sb = new StringBuffer();
+                for (int j = 0; j < r.length; j++) {
+                    sb.append(r[j]);
+                    sb.append(" ");
+                }
+                result.add(sb.toString().trim());
+                subselect(a, i + 1, index + 1, r, k, result);
+            } else {
+                return result;
+            }
+        }
+        return result;
+    }
+}
