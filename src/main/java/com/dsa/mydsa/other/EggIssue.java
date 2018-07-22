904da5d905ea825de351985836fa9e72e0021ee8
zzb <columbia7311@163.com>
2018-07-22 18:15:02 +0800

    add egg issue

--- src/main/java/com/dsa/mydsa/other/EggIssue.java
+++ src/main/java/com/dsa/mydsa/other/EggIssue.java
@@ -8,6 +8,7 @@ public class EggIssue {
     public static void main(String[] args) {
         System.out.println(findNum1(2, 100));
         System.out.println(findFlood(2, 100));
+        System.out.println(findFloorBetter(2, 100));
     }
 
     public static int findNum1(int N, int M) {
@@ -50,5 +51,22 @@ public class EggIssue {
             }
         }
         return cache[N][M];
+    }
+
+    public static int findFloorBetter(int N, int M) {
+        int[] preresult = new int[M + 1];
+        int[] result = new int[M + 1];
+        for (int i = 0; i < M + 1; i++) {
+            result[i] = i;
+        }
+        for (int i = 2; i <= N; i++) {
+            System.arraycopy(result, 0, preresult, 0, result.length);
+            for (int j = 1; j <= M; j++) {
+                for (int k = 1; k < j; k++) {
+                    result[j] = Math.min(result[j], 1 + Math.max(preresult[k - 1], result[j - k]));
+                }
+            }
+        }
+        return result[M];
     }
 }
