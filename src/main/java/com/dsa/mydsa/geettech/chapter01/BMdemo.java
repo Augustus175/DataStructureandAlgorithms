58db5acbf25aaaeca6c7ed96aa23fbd1da4f3efc
Augustus175 <columbia7311@163.com>
2019-05-29 22:37:18 +0800

    2019-05-29 22:36:40

--- src/main/java/com/dsa/mydsa/geettech/chapter01/BMdemo.java
+++ src/main/java/com/dsa/mydsa/geettech/chapter01/BMdemo.java
@@ -2,4 +2,11 @@ package com.dsa.mydsa.geettech.chapter01;
 
 
 public class BMdemo {
+    public int pattern(String text, String pattern) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
