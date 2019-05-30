c0ef1fc374550e04570fb421f96e16fec7cec104
Augustus175 <columbia7311@163.com>
2019-05-30 23:12:21 +0800

    2019-05-30 23:11:44

--- src/main/java/com/dsa/mydsa/geettech/chapter01/BMdemo.java
+++ src/main/java/com/dsa/mydsa/geettech/chapter01/BMdemo.java
@@ -2,11 +2,31 @@ package com.dsa.mydsa.geettech.chapter01;
 
 
 public class BMdemo {
-    public int pattern(String text, String pattern) {
+    public static int pattern(String text, String pattern) {
+        int i = 0;
+        int m = pattern.length();
+        while (i <= text.length() - m) {
+            int j;
+            int k = i + m - 1;
+            for (j = m - 1; j >= 0 && text.charAt(k--) == pattern.charAt(j); j--) {
+                System.out.println(j);
+            }
+            ;
+            if (j < 0) {
+                return i;
+            } else {
+                i += step();
+            }
+        }
         return -1;
     }
 
-    public static void main(String[] args) {
+    private static int step() {
+        return 1;
+    }
 
+    public static void main(String[] args) {
+        int pattern = pattern("abcabcabcaaa", "aaa");
+        System.out.println(pattern);
     }
 }
